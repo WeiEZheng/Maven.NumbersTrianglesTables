0291a6eec86f67ab8a9808cc11e783b31f8c3f0a
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-02-24 21:37:09 -0500

    did squared, even, and odd

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,17 +3,35 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String evennumbers="";
+        for (int i=start;i<stop;i++){
+            if (i%2==0){
+                evennumbers = evennumbers + i;
+            }
+        }
+        return evennumbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String oddnumbers="";
+        for (int i=start;i<stop;i++){
+            if (i%2==1){
+                oddnumbers = oddnumbers + i;
+            }
+        }
+        return oddnumbers;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squarednumber ="";
+        int storage;
+        for (int i=start;i<stop;i+=step){
+            storage = (int) Math.pow(i,2);
+            squarednumber = squarednumber + storage;
+            }
+        return squarednumber;
     }
 
     public static String getRange(int start) {
