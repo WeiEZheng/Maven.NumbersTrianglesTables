cb362f7515e773b35eca4c65b74b3e493db787a9
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-01 15:31:50 -0500

    reformated tables to fit test

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,41 +2,20 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        String table="";
-        for (int i=1;i<=4;i++){
-            for (int j=1; j<=4;j++){
-                if (j==1)
-                    table=table+"  "+(j*i);
-                else
-                    table=table+" |  "+(j*i);
-            }
-            table=table+"\n";
-        }
-        return table;
+        return getMultiplicationTable(5);
+        //Readme says 4 by 4 but sample output is 5 by 5 and same with test
     }
 
     public static String getLargeMultiplicationTable() {
-        String table="";
-        for (int i=1;i<=9;i++){
-            for (int j=1; j<=9;j++){
-                if (j==1)
-                    table=table+"  "+(j*i);
-                else
-                    table=table+" |  "+(j*i);
-            }
-            table=table+"\n";
-        }
-        return table;
+        return getMultiplicationTable(10);
+        //Readme says 9 by 9 but sample output is 10 by 10 and same with test
     }
 
     public static String getMultiplicationTable(int tableSize) {
         String table="";
         for (int i=1;i<=tableSize;i++){
             for (int j=1; j<=tableSize;j++){
-                if (j==1)
-                    table=table+"  "+(j*i);
-                else
-                    table=table+" |  "+(j*i);
+                table=table+String.format("%3d |",j*i);
             }
             table=table+"\n";
         }
